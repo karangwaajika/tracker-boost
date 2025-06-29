f92c090541c23bf5f5dc029dad2206e3ad1877ea
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-06-29 09:53:47 +0200

    ref: return project response dto instead of entity

--- src/main/java/com/lab/trackerboost/service/ProjectService.java
+++ src/main/java/com/lab/trackerboost/service/ProjectService.java
@@ -1,6 +1,7 @@
 package com.lab.trackerboost.service;
 
 import com.lab.trackerboost.dto.project.ProjectRequestDto;
+import com.lab.trackerboost.dto.project.ProjectResponseDto;
 import com.lab.trackerboost.model.ProjectEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,12 +10,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProjectService {
-    ProjectEntity create(ProjectRequestDto project);
+    ProjectRequestDto create(ProjectRequestDto project);
     Optional<ProjectEntity> findProjectById(Long id);
     Optional<ProjectEntity> findProjectByName(String name);
-    Page<ProjectEntity> findAll(Pageable pageable);
-    ProjectEntity partialUpdate(ProjectEntity project, Long id);
+    Page<ProjectResponseDto> findAll(Pageable pageable);
+    ProjectRequestDto partialUpdate(ProjectRequestDto project, Long id);
     void deleteById(Long id);
-    List<ProjectEntity> findAllProject();
-    List<ProjectEntity> findProjectsWithoutTasks();
+    List<ProjectResponseDto> findAllProject();
+    List<ProjectResponseDto> findProjectsWithoutTasks();
 }
