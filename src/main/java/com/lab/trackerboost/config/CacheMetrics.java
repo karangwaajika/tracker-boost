72f03d655c23087ed0fb9ea8efdf8f5866fcd467
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-06-29 15:58:19 +0200

    feat: set counter for caches hit or missed

---
+++ src/main/java/com/lab/trackerboost/config/CacheMetrics.java
@@ -0,0 +1,23 @@
+package com.lab.trackerboost.config;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
+public class CacheMetrics {
+
+    private final Counter cacheHits;
+    private final Counter cacheMisses;
+
+    public CacheMetrics(MeterRegistry meterRegistry) {
+        this.cacheHits = meterRegistry.counter("cache_hits_total");
+        this.cacheMisses = meterRegistry.counter("cache_misses_total");
+    }
+
+    public void incrementCacheHit() {
+        cacheHits.increment();
+    }
+
+    public void incrementCacheMiss() {
+        cacheMisses.increment();
+    }
+}
